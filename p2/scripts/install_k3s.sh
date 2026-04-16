94226ec10bfca42032581c79fcdd1c9b0953d769
aelfany <abdalltifalfani309@gmail.com>
2026-04-16 11:38:53 +0100

    change name P1,P2,P3 to p1,p2,p3 and in p2 give kubectl global permission 644

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode 644" sh -
 sleep 10
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl apply -f /vagrant/confs/
